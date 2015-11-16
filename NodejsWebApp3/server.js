644da3693303c31a0675286dafbdff0f812b22f3
dwkim <daewoo.kim@microsoft.com>
2015-11-16 19:52:34 +0900

    thrid

--- NodejsWebApp3/server.js
+++ NodejsWebApp3/server.js
@@ -2,5 +2,5 @@
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World good\n');
+    res.end('Hello World bad\n');
 }).listen(port);
